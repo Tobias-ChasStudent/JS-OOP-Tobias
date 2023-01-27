4710a2b79e753c296b9e72e8dc881c120afe5f6b
Tobias-ChasStudent <tobias.riiser@chasacademy.se>
2023-01-27 17:45:48 +0100

    Part 4 done and dusted

--- poker/poker.js
+++ poker/poker.js
@@ -10,6 +10,7 @@ class cardDeck {
     generateDeck() {
         let suits = ["diamonds", "spades", "hearts", "clubs"]
         let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+        let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
 
         suits.forEach(suit => {
             values.forEach(value => {
@@ -17,6 +18,7 @@ class cardDeck {
                     readable: `${value}_of_${suit}`,
                     value: value,
                     suit: suit,
+                    number: number[values.indexOf(value)]
                 }
                 //console.log(card);
                 this.cardDeck.push(card)
@@ -42,6 +44,7 @@ class pokerPlayer {
         this.name = name
         this.cardHand = []
     }
+
     dealCards(handSize, deckName) {
         for (let i = 0; i < handSize; i++) {
             let cardAdd = deckName.cardDeck.pop()
@@ -51,19 +54,22 @@ class pokerPlayer {
         console.log(`${this.name}'s hand after being dealt: `);
         console.log(this.cardHand);
         this.updateCardHtml()
-        
+
         h3CardDeck.textContent = "Cards left in the deck: " + deckName.cardDeck.length
 
+
+
     }
+
     discardCard(cardIndex, deckName) {
         //Convert cardHand into a JSON array, then convert it back
         //Why does this work? HAHAHAHAHAHAHAHAHAAHAHAHAHAHAHAHAHHAH
         console.log(this.cardHand);
         console.log(this.cardHand[cardIndex]);
         /* console.log(deckName.cardDeck.pop()); */
-        
+
         deckName.discardPile.push(this.cardHand[cardIndex]);
-        h3DiscardPile.textContent = "Discard pile size: "+deckName.discardPile.length
+        h3DiscardPile.textContent = "Discard pile size: " + deckName.discardPile.length
 
         this.cardHand[cardIndex] = deckName.cardDeck.pop()
         console.log(this.name + "'s new hand after removing a card:");
@@ -72,39 +78,96 @@ class pokerPlayer {
 
         h3CardDeck.textContent = "Cards left in the deck: " + deckName.cardDeck.length
     }
+
+    discardAllCards(deckName) {
+        console.log("Discarded all cards");
+        for (let i = this.cardHand.length; i > 0; i--) {
+
+            const card = this.cardHand[i - 1];
+            console.log(card);
+            deckName.discardPile.push(this.cardHand.pop())
+
+        }
+        //console.log(this.cardHand);
+        h3DiscardPile.textContent = "Discard pile size: " + deckName.discardPile.length
+        this.updateCardHtml()
+
+    }
+
     updateCardHtml() {
         let divCardHand = ""
+        let handValue = 0
         if (document.querySelector(`#${this.name}`)) {
-            divCardHand = document.querySelector(`#${this.name}`) 
+            divCardHand = document.querySelector(`#${this.name}`)
             divCardHand.innerHTML = ""
         } else {
             divCardHand = document.createElement("div")
         }
+        let h2Player = document.createElement("h2")
+        h2Player.textContent = this.name
+        divCardHand.appendChild(h2Player)
+
         divCardHand.id = this.name
         this.cardHand.forEach(card => {
+            handValue += parseInt(card.number)
             let imgCard = document.createElement("img")
-            imgCard.setAttribute("src", "./img/"+card.readable+".png")
-            imgCard.setAttribute("width", 100)  
+            imgCard.setAttribute("src", "./img/" + card.readable + ".png")
+            imgCard.setAttribute("width", 100)
             divCardHand.appendChild(imgCard);
         });
+
+        let h4HandValue = document.createElement("h4")
+        h4HandValue.textContent = `Hand value: ${handValue}`
+        divCardHand.appendChild(h4HandValue);
+
+        divCardHand.appendChild(document.createElement("hr"))
+
         console.log(divCardHand);
         document.body.appendChild(divCardHand)
     }
 }
 
 btnRestart.addEventListener("click", () => {
-    pokerDeck.generateDeck()
+    
+    btnRestart.disabled = true
+
+    playerSlim.discardAllCards(pokerDeck)
+    playerLuke.discardAllCards(pokerDeck)
+    
+    setTimeout(() => {
+        for (let i = pokerDeck.discardPile.length; i > 0; i--) {
+            const card = pokerDeck.discardPile[i - 1];
+            console.log(card);
+
+            pokerDeck.cardDeck.push(pokerDeck.discardPile.pop())
+
+            h3DiscardPile.textContent = "Discard pile size: " + pokerDeck.discardPile.length
+
+            h3CardDeck.textContent = "Cards left in the deck: " + pokerDeck.cardDeck.length
+
+            pokerDeck.shuffleDeck()
+
+        }
+    }, 500);
+
+    setTimeout(() => {
+        playerSlim.dealCards(5, pokerDeck)
+        playerLuke.dealCards(5, pokerDeck)
+        btnRestart.disabled = false
+    }, 1000);
+
+
 })
 
-const pokerDeck = new cardDeck()
+let pokerDeck = new cardDeck()
 
 pokerDeck.generateDeck()
 
 pokerDeck.shuffleDeck()
 
 //Player Slim and Luke
-const playerSlim = new pokerPlayer("Slim")
-const playerLuke = new pokerPlayer("Luke")
+let playerSlim = new pokerPlayer("Slim")
+let playerLuke = new pokerPlayer("Luke")
 
 playerSlim.dealCards(5, pokerDeck)
 playerLuke.dealCards(5, pokerDeck)
@@ -121,4 +184,8 @@ setTimeout(() => {
 }, 1000);
 
 
-console.log(pokerDeck.cardDeck);
+    
+
+
+
+//console.log(pokerDeck.cardDeck);
